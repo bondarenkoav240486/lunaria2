9f90a571399f9da02365a196d5781fc7fae47d39
bondarenkoav240486 <bondarenko.av@gmail.com>
2023-07-14 20:24:35 +0300

    commit1 push project to host

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {jquery} from '../js/jquery-3.6.3.min.js';
+import {$} from '../js/jquery-3.6.3.min.js';
+import {scripts} from '../js/scripts.js';
+import {slick} from '../js/slick.min.js';
+import {sliderSlick} from '../js/slidersSlick.js';
+
